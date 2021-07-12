1827a0c6bc203a5165ea3a77723d6c8e4226f5b3
SistemasEmbebidos <78398897+SistemasEmbebidos2020@users.noreply.github.com>
2021-07-12 13:54:44 -0500

    Create blinkled2.py

---
+++ blinkled2.py
@@ -0,0 +1,42 @@
+
+import RPi.GPIO as GPIO
+from time import sleep
+
+led1 = 4
+led2 = 17
+bt1 = 5
+bt2 = 6
+
+def peripheral_setup():
+ GPIO.setmode(GPIO.BCM)
+ GPIO.setup(led1, GPIO.OUT)
+ GPIO.setup(bt1, GPIO.IN, GPIO.PUD_DOWN)
+ GPIO.setup(led2, GPIO.OUT)
+ GPIO.setup(bt2, GPIO.IN, GPIO.PUD_UP)
+
+def peripheral_loop():
+ if GPIO.input(bt1):
+  GPIO.output(led1,True)
+ else:
+  GPIO.output(led1,False)
+ if GPIO.input(bt2):
+  GPIO.output(led2,False)
+ else:
+  GPIO.output(led2,True)
+
+ 
+# Main function
+def main () :
+
+# Setup
+ peripheral_setup()
+
+# Infinite loop
+ while 1 :
+  peripheral_loop()
+  pass
+
+# Command line execution
+if __name__ == '__main__' :
+   main()
+
