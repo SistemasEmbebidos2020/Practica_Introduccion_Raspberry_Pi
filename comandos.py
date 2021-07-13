aafe0f9ce2de3de340e5060626f3626addb3d3de
SistemasEmbebidos <78398897+SistemasEmbebidos2020@users.noreply.github.com>
2021-07-13 13:53:14 -0500

    Create comandos.py

---
+++ comandos.py
@@ -0,0 +1,24 @@
+# !/usr/bin/env python3
+#!/usr/bin/env python3
+
+import subprocess
+import sys
+import os
+import nano 
+
+#subprocess.check_call([sys.executable, '-m', 'pip', 'install', 'nano'])
+#subprocess.check_call([sys.executable, '-m', 'pip', 'install', '--upgrade','pip'])
+def loop():
+ pass
+
+def main():
+ 
+ subprocess.call(['ls', '/'])
+ subprocess.call(['pwd'], shell=False)
+ subprocess.call(['touch', 'prueba1.py'])
+ subprocess.call(['ls'], shell=False)
+ while(True):
+  loop()
+
+if  __name__ ==  '__main__':
+ main()
