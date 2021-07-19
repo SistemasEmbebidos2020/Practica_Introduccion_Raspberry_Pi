6686190e10d8bb3dea0bd29b11551f2af363c94e
SistemasEmbebidos <78398897+SistemasEmbebidos2020@users.noreply.github.com>
2021-07-19 08:32:20 -0500

    Create pwmbase.py

---
+++ pwmbase.py
@@ -0,0 +1,12 @@
+import RPi.GPIO as GPIO
+from time import*
+GPIO.setmode(GPIO.BCM)
+PPWM = 12
+GPIO.setup(PPWM, GPIO.OUT)
+servo= GPIO.PWM(PPWM, 50) #Para crear una instancia de PWM (Canal,frecuencia)
+servo.start(0) #inicilizar valor en dc del PWM
+
+
+while True:
+ servo.ChangeDutyCycle(25) # Para cambiar el ciclo de trabajo de 0 a 100
+ sleep(0.02)
