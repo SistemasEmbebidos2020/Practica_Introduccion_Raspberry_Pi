e3d215b852dbe6d313186e08a3a493b617acb292
SistemasEmbebidos <78398897+SistemasEmbebidos2020@users.noreply.github.com>
2022-07-26 12:20:37 -0500

    Update pwm.py

--- Presencial/pwm.py
+++ Presencial/pwm.py
@@ -7,17 +7,17 @@ GPIO.setmode(GPIO.BCM)
 
 PPWM = 12
 GPIO.setup(PPWM, GPIO.OUT)
-servo= GPIO.PWM(PPWM, 50) #Para crear una instancia de PWM (Canal,frecuencia)
-servo.start(0) #inicilizar valor en dc del PWM
+led= GPIO.PWM(PPWM, 50) #Para crear una instancia de PWM (Canal,frecuencia)
+led.start(0) #inicilizar valor en dc del PWM
 t=0
 try:
  while True:
   while t < 100:
-   servo.ChangeDutyCycle(t) # Para cambiar el ciclo de trabajo de 0 a 100
+   led.ChangeDutyCycle(t) # Para cambiar el ciclo de trabajo de 0 a 100
    t+=5
    sleep(0.2)
 while t > 0:
-   servo.ChangeDutyCycle(t) # Para cambiar el ciclo de trabajo de 0 a 100
+   led.ChangeDutyCycle(t) # Para cambiar el ciclo de trabajo de 0 a 100
    t-=5
    sleep(0.2)
 except:
