3e87d0a1106172619321aaa38cd89f892216cbd2
SistemasEmbebidos <78398897+SistemasEmbebidos2020@users.noreply.github.com>
2022-07-26 08:55:05 -0500

    Create blink1.py

---
+++ Presencial/blink1.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+
+import RPi.GPIO as GPIO
+from time import sleep
+GPIO.setwarnings(False)
+
+def peripheral_setup():
+ GPIO.setmode(GPIO.BCM) #puede cambiar a BOARD
+ global led1
+ led1 = 17  #si cambiar de BCM a Board defina el número del pin acorde a los pines de la raspberry
+ GPIO.setup(led1, GPIO.OUT)
+ 
+def peripheral_loop():
+ GPIO.output(led1,True)
+ sleep(2)
+ GPIO.output(led1,False)
+ sleep(2)
+
+# Main function
+def main () :
+
+# Setup
+ peripheral_setup()
+ try:
+# Infinite loop
+  while 1 :
+   peripheral_loop()
+ except(KeyboardInterrupt):
+  print()
+  print("bye")
+  GPIO.cleanup()
+ 
+main()
