081b8c33c72ddafb02824b461f5cc107d5bc955b
SistemasEmbebidos <78398897+SistemasEmbebidos2020@users.noreply.github.com>
2022-07-26 11:16:50 -0500

    Update timeInterump.py

--- Presencial/timeInterump.py
+++ Presencial/timeInterump.py
@@ -1,7 +1,9 @@
-
+import sys
 import RPi.GPIO as GPIO
-from delay import *
 from threading import *
+from delays.delay import *
+from time import *
+
 GPIO.setwarnings(False)
 
 led1 = 17
@@ -9,24 +11,26 @@ led2 = 27
 
 
 def int1():
- GPIO.output(led1,not(GPIO.input(led1)))
- Timer(2,int1).start()
+ state =  not(GPIO.input(led1))
+ GPIO.output(led1,state)
+ Timer(1,int1).start()
  
 def int2():
  GPIO.output(led2,not(GPIO.input(led2)))
- Timer(4,int2).start()
+ Timer(3,int2).start()
  
 def peripheral_setup():
  GPIO.setmode(GPIO.BCM)
  GPIO.setup(led1, GPIO.OUT)
  GPIO.setup(led2, GPIO.OUT)
- Timer(2,int1).start()
- Timer(4,int2).start()
+ Timer(1,int1).start()
+ Timer(3,int2).start()
 
 def peripheral_loop():
  print("hola mundo")
  delay(2)
- 
+ #sleep(2)
+
 # Main function
 def main () :
 
@@ -38,9 +42,11 @@ def main () :
 # Infinite loop
   while 1 :
    peripheral_loop()
- except:
+ except : #RuntimeError :  
+  Timer(1,int1)._delete() #cancel()
   print()
   print("Bye")
-  GPIO.cleaup()
+  sys.exit()
 
 main()
+GPIO.cleanup()
